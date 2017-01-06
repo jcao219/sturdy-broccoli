c7ba1ac2136bf1598c3373258f85b4563c7b5ca4
Jimmy Cao <jc2462@cornell.edu>
2017-01-05 23:30:45 -0600

    Fix typo

--- sturdy-broccoli.go
+++ sturdy-broccoli.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
-	_ "github.com/gonum/graph"
-	_ "github.com/gonum/graph/simple"
-	"log"
+	"github.com/gonum/graph/simple"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func parse_graph(s string) (*simple.UndirectedGraph, error) {
+	g := simple.NewUndirectedGraph(0.0, 0.0)
+	for _, edge := range strings.Split(s, "  ") {
+		pair := strings.Split(edge, " ")
+		if len(pair) != 2 {
+			return nil, errors.New("Bad format.")
+		}
+		v1, err1 := strconv.Atoi(pair[0])
+		v2, err2 := strconv.Atoi(pair[1])
+		if err1 != nil || err2 != nil {
+			return nil, errors.New("Bad format.")
+		}
+		g.SetEdge(simple.Edge{F: simple.Node(v1), T: simple.Node(v2), W: 0.0})
+	}
+	return g, nil
+}
 
 func main() {
 	args := os.Args
@@ -22,16 +40,25 @@ func main() {
 	}
 	file, err := os.Open(args[1])
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		var g *simple.UndirectedGraph
+		var err error
+		g, err = parse_graph(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(count_subgraphs(g))
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
